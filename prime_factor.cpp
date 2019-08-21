2498c0a50c66cb0030694564c01915fe03997f80
清水俊樹 <shimizutoshiki@MBA-3.local>
2019-08-21 23:12:10 +0900

    prime_factor

---
+++ prime_factor.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+map<long long, int> PrimeFactor(int n){
+    map<long long, int> ret;
+    for (int i=2;i*i<=n;i++){
+        while(n%i==0){
+            ret[i]++;
+            n/=i;
+        }
+    }
+    if (n!=1){
+        ret[n] = 1;
+    }
+    return ret;
+}
+
+int main() {
+  int N;
+  cin >> N;
+  for(auto p : PrimeFactor(N)) {
+    cout << p.first << " " << p.second << endl;
+  }
+}
