328b61e3135423a631cb435c6a76020d9a198d83
清水俊樹 <shimizutoshiki@MBA-3.local>
2019-08-31 17:48:39 +0900

    template.cpp

---
+++ template.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  FOR(i,0,n)
+#define ALL(a)  (a).begin(),(a).end()
+#define SORT(a) sort(ALL(a))
+#define dump(x)  cerr << #x << " = " << (x) << endl;
+typedef long long ll;
+const int INF = 999999999;
+const int MOD = (int)1e9 + 7;
+const int EPS = 1e-9;
+
+int main() {
+    vector<int> vec;
+    vec.push_back(1);
+    vec.push_back(3);
+    vec.push_back(2);
+    SORT(vec);
+    REP(i,vec.size()){
+        dump(vec[i]);
+    }
+}
