755af2d00ecc49382f3b06caff401d3c211ef3e2
清水俊樹 <shimizutoshiki@MBA-3.local>
2019-08-31 18:08:08 +0900

    sum template

--- template.cpp
+++ template.cpp
@@ -3,6 +3,7 @@ using namespace std;
 #define FOR(i,a,b) for(int i=(a);i<(b);++i)
 #define REP(i,n)  FOR(i,0,n)
 #define ALL(a)  (a).begin(),(a).end()
+#define SUM(a)  accumulate((a).begin(),(a).end(),0)
 #define SORT(a) sort(ALL(a))
 #define dump(x)  cerr << #x << " = " << (x) << endl;
 typedef long long ll;
