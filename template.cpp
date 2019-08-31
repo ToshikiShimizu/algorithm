354382790b34041f649aa42ffca2f7fb5b908eb0
清水俊樹 <shimizutoshiki@MBA-3.local>
2019-08-31 20:46:19 +0900

    algorithm REV

--- template.cpp
+++ template.cpp
@@ -5,6 +5,7 @@ using namespace std;
 #define ALL(a)  (a).begin(),(a).end()
 #define SUM(a)  accumulate((a).begin(),(a).end(),0)
 #define SORT(a) sort(ALL(a))
+#define REV(a) reverse(ALL(a))
 #define dump(x)  cerr << #x << " = " << (x) << endl;
 typedef long long ll;
 const int INF = 999999999;
