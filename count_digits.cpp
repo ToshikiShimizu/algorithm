c8fd95ecba48d522371201fa9b29c74f6db25fb8
清水俊樹 <shimizutoshiki@MBA-3.local>
2019-09-04 20:40:23 +0900

    count_digits

---
+++ count_digits.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  FOR(i,0,n)
+#define ALL(a)  (a).begin(),(a).end()
+#define SUM(a)  accumulate((a).begin(),(a).end(),0)
+#define SORT(a) sort(ALL(a))
+#define REV(a) reverse(ALL(a))
+#define dump(x)  cerr << #x << " = " << (x) << endl;
+typedef long long ll;
+const int INF = 999999999;
+const int MOD = (int)1e9 + 7;
+const int EPS = 1e-9;
+
+int CountDigits(ll n){
+   if (n==0)return 0;
+   int sum = 0;
+   while(n>0){
+       sum ++;
+       n /= 10;
+   }
+   return sum;
+}
+
+int main() {
+   cin.tie(0);
+   ios::sync_with_stdio(false);
+   ll n;
+   cin >> n;
+   cout << CountDigits(n) << endl;
+}
